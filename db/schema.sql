88c34b5140105e0bf8c19555c1e1f297a825c331
Nico (Filipu) Guarino <nizoworld@gmail.com>
2021-10-15 17:43:43 -0700

    commiting to use askBCs for help

--- db/schema.sql
+++ db/schema.sql
@@ -3,9 +3,7 @@ DROP TABLE IF EXISTS role;
 DROP TABLE IF EXISTS employee;
 
 CREATE DATABASE employee_tracker;
-
 USE employee_tracker;
-
 
 CREATE TABLE department (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
@@ -26,7 +24,7 @@ CREATE TABLE employee (
     last_name VARCHAR(30) NOT NULL,
     role_id INTEGER,
     manager_id INTEGER,
-    FOREIGN KEY (role_id) REFERENCES role(title) ON DELETE CASCADE
+    FOREIGN KEY (role_id) REFERENCES role(title) ON DELETE CASCADE,
     FOREIGN KEY (manager_id) REFERENCES role(id) ON DELETE CASCADE
 );
 
