59e0dbc3a464c8ad9977940160de17ff3f5990d6
Nico (Filipu) Guarino <nizoworld@gmail.com>
2021-10-16 15:43:53 -0700

    linked methods to switch statement

--- index.js
+++ index.js
@@ -26,31 +26,31 @@ function init() {
    .then((selectOption) => {
        switch (selectOption) {
             case "View All Employees":
-                
+                employee.listAllEmployees();
                 init();
                 break;
             case "View All Departments":
-                
+                department.listAllDepartments();
                 init();
                 break;
             case "View All Roles":
-            
+                role.listAllRoles();
                 init();
                 break;
              case "Add Employee":
-            
+                employee.addEmployee();
                 init();
                 break;
             case "Add Department":
-            
+                department.addDepartment();
                 init();
                 break;
             case "Add Role":
-            
+                role.addRole();
                 init();
                 break;
             case "Update Employee":
-            
+
                 init();
                 break;
             case "Exit":
@@ -82,3 +82,4 @@ function updateEmployee() {
     
 }
 
+init();
